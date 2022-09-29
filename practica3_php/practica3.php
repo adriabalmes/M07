da8ca9bc58827c67ed2852bb0fdb1c7d57fe1ad2
sebastian11223 <mcasco@jaumebalmes.net>
2022-09-29 20:28:57 +0200

    SEGUNDA PARTE TRABAJO

--- practica3_php/practica3.php
+++ practica3_php/practica3.php
@@ -1,35 +1,83 @@
 
-    <?php
+    
+     <?php
     
     /* clase vehicle */
-    class vehicle {
-        public $marca
-        public $modelo
-        public $numPlazas
-        public $color
-        public $combustible
-        }
-
-        /* constructor vacío */
-        function __construct(){
-        }
-
-        /* constructor por defecto */
-        function __construct($marca, $modelo, $numPlazas, $color, $combustible) {
-            $this -> marca = $marca;
-            $this -> modelo = $modelo;
-            $this -> numPlazas = $numPlazas;
-            $this -> color = $color;
-            $this -> combustible = $combustible;            
-        }
+    class Vehicle {
+        private $marca;
+        private $modelo;
+        private $numplazas;
+        private $color;
+        private $combustible;
         
-        /* constructor alternativo */
-        function __construct($marca, $modelo, $numPlazas) {
-            $this -> marca = $marca;
-            $this -> modelo = $modelo;
-            $this -> numPlazas = $numPlazas;
-        }
 
-            
-    ?>
 
+    /* constructor vacío */
+    function __construct(){
+    }
+
+    /* constructor por defecto */
+    function __construct2($marca, $modelo, $numplazas, $color, $combustible) {
+        $this -> marca = $marca;
+        $this -> modelo = $modelo;
+        $this -> numplazas = $numplazas;
+        $this -> color = $color;
+        $this -> combustible = $combustible;            
+    }
+    
+    /* constructor alternativo */
+    function __construct3($marca, $modelo, $numplazas) {
+        $this -> marca = $marca;
+        $this -> modelo = $modelo;
+        $this -> numplazas = $numplazas;
+    }
+    /* getter marca */
+    public function getMarca () {
+        return $this -> marca; 
+    } 
+    /* setter marca */
+    public function setmarca ($marca) {
+        $this -> marca = $marca; 
+    } 
+    /* getter modelo */
+    public function getmodelo () {
+        return $this -> modelo; 
+    } 
+    /* setter modelo */
+    public function setmodelo ($modelo) {
+        $this -> modelo = $modelo; 
+    }
+    /* getter numplazas */
+    public function getnumplazas () {
+        return $this -> numplazas; 
+    } 
+    /* setter numplazas */
+    public function setnumplazas ($numplazas) {
+        $this -> numplazas = $numplazas; 
+    } 
+    /* getter color */
+    public function getcolor () {
+        return $this -> color; 
+    } 
+    /* setter color */
+    public function setcolor ($color) {
+        $this -> color = $color; 
+    } 
+    /* getter combustible */
+    public function getcombustible () {
+        return $this -> combustible; 
+    } 
+    /* setter combustible */
+    public function setcombustible ($combustible) {
+        $this -> combustible = $combustible; 
+    } 
+        /* self::class */
+    
+        public function __toString () {
+        return "Marca del vehículo: ".$this->marca. " "."Modelo del vehículo: ".$this->modelo;
+    }
+       
+
+}
+
+?>
