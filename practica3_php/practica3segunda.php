3e0b14e0148e884c3d2da04a294166097176a71c
adriabalmes <aberruez@jaumebalmes.net>
2022-10-13 18:47:56 +0200

    subida con retraso por problemas tecnicos

--- practica3_php/practica3segunda.php
+++ practica3_php/practica3segunda.php
@@ -1,25 +1,16 @@
-
-    
-     <?php
-    
+<?php
     /* clase Compte */
     class Compte {
         private $nombre = "Alberto";
         private $apellidos = "Morales Sanz";
-        private $saldo = 5100;
-        
-
-        function dollars (){
-            if () {
-
-            }
-        }
+        private $saldo = 5000;
+ 
     /* constructor vacío */
-    function __construct(){
+    function __construct2(){
     }
 
     /* constructor por defecto */
-    function __construct2($nombre, $apellidos, $saldo) {
+    function __construct($nombre, $apellidos, $saldo) {
         $this -> nombre = $nombre;
         $this -> apellidos = $apellidos;
         $this -> saldo = $saldo;           
@@ -29,13 +20,38 @@
     function __construct3($saldo) {
         $this -> saldo = $saldo;
     }
-        /* self::class */
-    
-        public function __toString () {
-        return "Marca del vehículo: ".$this->marca. " "."Modelo del vehículo: ".$this->modelo;
+ 
+    function retirarDinero ($cantidadRetirada) {
+        return $newsaldo = $this->saldo - $cantidadRetirada;
     }
-       
 
+    function ingresarDinero ($cantidadIngresada) {
+        return $newsaldo = $this->saldo + $cantidadIngresada;
+    }
+
+    public function getsaldo() {
+        return $this->saldo;
+    }
+
+    public function setsaldo($saldo) {
+        $this->$saldo;
+    }
 }
+
+// Prueba por lectura de teclado, pero no funciona.
+//$cantidadRetirada = readline("Cúanto dinero quieres retirar?");
+//$cliente = new Compte("Adolfo", "Mith", 1000);
+
+    $cliente = new Compte($nombre, $apellidos, $saldo);
+    $cliente -> getsaldo();
+    $cantidadRetirada = 400;
+    echo nl2br("El saldo inicial es de: ".$cliente -> getsaldo()."\n");
+    echo nl2br ("La cantidad a retirar es de: ".$cantidadRetirada."\n");
+    echo nl2br ("El saldo actual ahora es de: ".$cliente->retirarDinero($cantidadRetirada)."\n");
+    echo "<br>";
+    $cantidadIngresada = 500;
+    echo nl2br("El saldo inicial es de: ".$cliente -> getsaldo()."\n");
+    echo nl2br ("La cantidad a ingresar es de: ".$cantidadIngresada."\n");
+    echo nl2br ("El saldo actual ahora es de: ".$cliente->ingresarDinero($cantidadIngresada)."\n");
 
 ?>
